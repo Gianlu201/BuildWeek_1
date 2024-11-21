296d4f4c3ed9e5dc2205ec4513edbd17913d3c4f
Gianlu201 <gianlucadidiego2001@gmail.com>
2024-11-21 23:50:38 +0100

    extra per mostrare resoconto quiz fatto

--- assets/js/results.js
+++ assets/js/results.js
@@ -6,6 +6,10 @@ const wrongs = document.getElementById('wrongs');
 const corrects = document.getElementById('corrects');
 const ctx = document.getElementById('myChart').getContext('2d');
 const btnRateUs = document.getElementById('btnRateUs');
+const btnShowResults = document.getElementById('btnShowResults');
+const resultMask = document.getElementById('resultMask');
+const btnMask = document.getElementById('btnMask');
+const maskContent = document.getElementById('maskContent');
 
 //---------------------------------------------------------------
 
@@ -18,6 +22,7 @@ function init() {
   checkScore(lecture);
   printScore();
   graphicShow();
+  createQuizRecap(lecture);
 }
 
 // funzione di lettura del local storage
@@ -28,7 +33,7 @@ function fromLocalStorage() {
   return myObj2;
 }
 
-// funzione di confronto e calcolo del risultato 
+// funzione di confronto e calcolo del risultato
 
 function checkScore(obj) {
   for (let i = 0; i < obj.domande.length; i++) {
@@ -37,21 +42,23 @@ function checkScore(obj) {
     } else {
       sbagliate++;
     }
-  };
+  }
   if (giuste >= 6) {
     result = `CONGRATULATION, you passed!`;
   } else {
-    result = 'SORRY, you didn\'t pass!';
+    result = "SORRY, you didn't pass!";
   }
 }
 
-// funzione di stampa del risultato 
+// funzione di stampa del risultato
 
 function printScore() {
   wrongs.innerText = sbagliate;
   corrects.innerText = giuste;
-  document.getElementById('correctPercentage').innerText = giuste / lecture.domande.length * 100;
-  document.getElementById('wrongPercentage').innerText = sbagliate / lecture.domande.length * 100;
+  document.getElementById('correctPercentage').innerText =
+    (giuste / lecture.domande.length) * 100;
+  document.getElementById('wrongPercentage').innerText =
+    (sbagliate / lecture.domande.length) * 100;
 }
 
 // funzione per creazione del grafico e inserimento dei dati del risultato
@@ -114,34 +121,56 @@ function graphicShow() {
 btnRateUs.addEventListener('click', function (event) {
   event.preventDefault();
   window.location.assign('feedback.html');
-
 });
 
+// funzione per far comparire la maschera con le domande quando viene premuto il bottone
+btnShowResults.addEventListener('click', function () {
+  resultMask.classList.toggle('hidden');
+});
 
+// funzione per far nascondere la maschera con le domande quando viene premuto il bottone
+btnMask.addEventListener('click', function () {
+  resultMask.classList.toggle('hidden');
+});
 
+// funzione per generare il recap del quiz
+function createQuizRecap(obj) {
+  for (let i = 0; i < obj.risposte.length; i++) {
+    const myDiv = document.createElement('div');
 
+    const myH4 = document.createElement('h4');
+    myH4.innerText = obj.domande[i].question;
+    myDiv.appendChild(myH4);
 
+    const myUl = document.createElement('ul');
+    for (let j = 0; j < obj.domande[i].incorrect_answers.length; j++) {
+      const myLi = document.createElement('li');
+      myLi.innerText = obj.domande[i].incorrect_answers[j];
+      myUl.appendChild(myLi);
+      if (checkMyAnswer(obj.domande[i].incorrect_answers[j], obj.risposte[i])) {
+        myLi.style =
+          'color: blueviolet; font-weight: bolder; font-size: 1.3em;';
+      }
+    }
+    const myLi = document.createElement('li');
+    myLi.innerText = obj.domande[i].correct_answer;
+    myLi.innerText += ` ✔️`;
+    if (checkMyAnswer(obj.domande[i].correct_answer, obj.risposte[i])) {
+      myLi.style = 'color: blueviolet; font-weight: bolder; font-size: 1.3em;';
+    }
 
+    myUl.appendChild(myLi);
+    myDiv.appendChild(myUl);
+    maskContent.appendChild(myDiv);
+    console.log(obj.domande[i].incorrect_answers.length);
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// funzione per verificare se la risposta che si sta passando corrisponde a quella data dall'utente
+function checkMyAnswer(actualAnswer, myAnswer) {
+  if (actualAnswer === myAnswer) {
+    return true;
+  } else {
+    return false;
+  }
+}
